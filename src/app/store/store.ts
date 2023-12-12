1452985e29828836622790d232ab9783ab3fb0ed
Margarita P <rprivalko@gmail.com>
2023-12-12 18:13:47 +0300

    add: add useAppDispatch & useAppSelector

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,8 +1,12 @@
-import {combineReducers} from 'redux';
-import {thunk} from 'redux-thunk';
+import {AnyAction, combineReducers} from 'redux';
+import {thunk, ThunkDispatch} from 'redux-thunk';
 import {configureStore} from '@reduxjs/toolkit';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
+import {weatherReducer} from '@/entities/ToolBox/ui/MainInfo/model';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+	weather: weatherReducer,
+});
 export const store = configureStore({
 	reducer: rootReducer,
 	middleware: getDefaultMiddleware =>
@@ -11,3 +15,9 @@ export const store = configureStore({
 
 
 export type AppRootStateType = ReturnType<typeof store.getState>
+
+export type AppDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
