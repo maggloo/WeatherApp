b27296c1b29b608ff869c6f151fbcc7c636310f4
Margarita P <rprivalko@gmail.com>
2023-12-12 18:14:17 +0300

    add: add Main.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import {Provider} from 'react-redux';
 import {store} from '@/app/store/store';
+import Main from '@/pages/Main';
 
 
 export default function App() {
+
 	return (
 		<Provider store={store}>
-			<></>
+			<Main />
 		</Provider>
 	);
 }
