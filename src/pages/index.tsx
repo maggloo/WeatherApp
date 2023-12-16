d7f6a3a3a3a16855e4948382c4eeff9dd8aec651
Margarita P <rprivalko@gmail.com>
2023-12-16 19:14:30 +0300

    fix: tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from '@/app/store/store';
 import Main from '@/pages/Main';
+import '../app/globals.css';
 
 
 export default function App() {
