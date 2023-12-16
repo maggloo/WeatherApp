20c1b1bbe6f349d3adf39586d83190fa30a9fe56
Margarita P <rprivalko@gmail.com>
2023-12-16 19:13:59 +0300

    add: main temp info

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,19 +1,23 @@
 import React, {useEffect} from 'react';
-import {useAppDispatch} from '@/app/store/store';
+import {useAppDispatch, useAppSelector} from '@/app/store/store';
 import {getSummaryWeather} from '@/entities/ToolBox/ui/MainInfo/model';
+import {mainSelector, weatherSelector} from '@/shared/selectors/weatherSelectors';
 
 const Main = () => {
 
 	const dispatch = useAppDispatch();
+	const weather = useAppSelector(weatherSelector);
+	const main = useAppSelector(mainSelector);
 
 	useEffect(() => {
 		dispatch(getSummaryWeather('Minsk'));
 	}, []);
-
+	const tempCalculation = (temp: number) => Math.round(temp);
 
 	return (
 		<div>
-            Main
+			{weather[0] && weather[0].main}
+			<h1 className={'text-3xl font-bold underline'} > {tempCalculation(main.temp)} </h1>
 		</div>
 	);
 };
