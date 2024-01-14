405e7a905616fa432b81f7aa88688e658e21b823
Margarita P <rprivalko@gmail.com>
2024-01-14 17:10:54 +0300

    add: precipitation info widget

--- src/entities/ToolBox/ui/PrecipitationInfo/ui/PrecipitationInfo.tsx
+++ src/entities/ToolBox/ui/PrecipitationInfo/ui/PrecipitationInfo.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {PrecipitationType} from '@/shared/types';
 
-const PrecipitationInfo = () => {
+type PrecipitationInfoPropsType = {
+	precipitation: PrecipitationType | undefined
+	typePrecip: string | undefined
+}
+const PrecipitationInfo: FC<PrecipitationInfoPropsType> = ({ precipitation, typePrecip }) => {
 	return (
 		<div>
-            Hello
+			<p className='font-semibold'>
+				{precipitation?.['1h'] || 0}mm <br></br>in the last 3h
+			</p>
+			<div className="mt-6 flex items-center pt-0 text-xs">
+				{typePrecip
+					? typePrecip === 'Snow'
+						? 'Snowy conditions.' : 'Rainy conditions.'
+					: 'Conditions are dry.'}
+			</div>
 		</div>
+
 	);
 };
 
--- src/widgets/Precipitation/ui/Precipitation.tsx
+++ src/widgets/Precipitation/ui/Precipitation.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ToolBox from '@/shared/ui/ToolBox/ToolBox';
 import PrecipitationInfo from '@/entities/ToolBox/ui/PrecipitationInfo/ui/PrecipitationInfo';
+import {useAppSelector} from '@/app/store/store';
+import {precipitationSelector} from '@/shared/selectors/weatherSelectors';
 
 const Precipitation = () => {
+
+	const precipitationInfo = useAppSelector(precipitationSelector);
+
+
+	const typePrecip = precipitationInfo && (precipitationInfo.rain ? 'Rain' : precipitationInfo.snow ? 'Snow' : undefined);
+
 	return (
 		<ToolBox header="Precipitation" className="order-6 flex h-48 flex-col justify-between">
-			<PrecipitationInfo/>
+			<PrecipitationInfo precipitation={precipitationInfo && (precipitationInfo.rain || precipitationInfo.snow)} typePrecip={typePrecip} />
 		</ToolBox>
 	);
 };
