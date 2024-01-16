b8356ef61b5f4ace45dc70383e5bb46c3f1bcb5d
Margarita P <rprivalko@gmail.com>
2024-01-16 16:43:53 +0300

    add: air pollution info widget

---
+++ src/widgets/Pollution/ui/AirPollution.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ToolBox from '@/shared/ui/ToolBox/ToolBox';
+import AirPollutionInfo from '@/entities/ToolBox/ui/AirPollutionInfo/ui/AirPollutionInfo';
+import {useAppSelector} from '@/app/store/store';
+import {airPollutionSelector} from '@/shared/selectors/weatherSelectors';
+
+const AirPollution = () => {
+	const pollutionData = useAppSelector(airPollutionSelector);
+
+	return (
+		<ToolBox header={'Air pollution'} className="order-2 col-span-2 flex h-48 flex-col justify-between">
+			<AirPollutionInfo pollutionData={pollutionData}/>
+		</ToolBox>
+	);
+};
+
+export default AirPollution;
