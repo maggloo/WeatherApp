7d4c92a18f955f5ccb86521dfd700b417da93827
Margarita P <rprivalko@gmail.com>
2024-01-18 17:20:10 +0300

    add: pressure

---
+++ src/widgets/Pressure/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ToolBox from '@/shared/ui/ToolBox/ToolBox';
+import {useAppSelector} from '@/app/store/store';
+import {pressureSelector} from '@/shared/selectors/weatherSelectors';
+import PressureInfo from '@/entities/ToolBox/ui/PressureInfo';
+
+const Pressure = () => {
+
+	const pressure = useAppSelector(pressureSelector);
+
+	return (
+		<ToolBox header={'Pressure'} className='order-10 flex h-48 flex-col justify-between'>
+			<PressureInfo pressure={pressure}/>
+		</ToolBox>
+	);
+};
+
+export default Pressure;
