a0c3db0e31184af8a894af761c00657172424243
Margarita P <rprivalko@gmail.com>
2024-01-18 17:20:33 +0300

    add: visibility

---
+++ src/widgets/Visibility/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ToolBox from '@/shared/ui/ToolBox/ToolBox';
+import {useAppSelector} from '@/app/store/store';
+import {visibilitySelector} from '@/shared/selectors/weatherSelectors';
+import VisibilityInfo from '@/entities/ToolBox/ui/VisibilityInfo';
+
+
+const Visibility = () => {
+
+	const visibility = useAppSelector(visibilitySelector);
+
+	return (
+		<ToolBox header={'Visibility'} className='order-9 flex h-48 flex-col justify-between'>
+			<VisibilityInfo visibility={visibility}/>
+		</ToolBox>
+	);
+};
+
+export default Visibility;
