8217854784d5757f608591671e981783a347f0ac
Margarita P <rprivalko@gmail.com>
2024-01-22 18:59:44 +0300

    refactor main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import MainWeather from '@/widgets/MainWeather';
+import {getSummaryWeather} from '@/entities/MainWeatherInfo/SummaryWeatherInfo/model';
+import {useAppDispatch} from '@/app/store/store';
 import WeatherWidgets from '@/widgets/WeatherWidgets';
 
 const Main = () => {
+
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(getSummaryWeather('Minsk'));
+	}, []);
+
+
 	return (
 		<div className="container mx-auto flex min-h-screen flex-col px-[1rem] antialiased md:px-[2rem]">
 			<div className="flex-grow">
