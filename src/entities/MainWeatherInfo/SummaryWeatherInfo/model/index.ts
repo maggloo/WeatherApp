5d1a0b27245a2815b89eaa4990afe632a36b6199
Margarita P <rprivalko@gmail.com>
2024-01-22 18:58:37 +0300

    refactor slice data organization

--- src/entities/MainWeatherInfo/SummaryWeatherInfo/model/index.ts
+++ src/entities/MainWeatherInfo/SummaryWeatherInfo/model/index.ts
@@ -1,87 +1,37 @@
 import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {weatherAPI} from '@/shared/api/weatherAPI';
 import {
-	WeatherType,
-	CurrentWeatherType, DailyWeatherType, HourlyWeatherType, WindType,
+	CurrentWeatherType, DailyWeatherType, HourlyWeatherType,
 } from '@/shared/types';
-import dayjs from 'dayjs';
 
 type initialStateType = {
-	weather: WeatherType[],
-	currentDate: string,
-	currentDay: string,
-	currentTime: string,
-	currentCity: string,
-	dt: number,
-	currentTemp: number,
-	sunrise: string,
-	sunset: string,
-	wind: WindType,
-	visibility: number,
-	forecastList: DailyWeatherType[],
-	hourlyWeather: HourlyWeatherType[],
-	airPollution: number,
-	uv: number,
-	feelsLike: number,
-	humidity: number,
-	pressure: number
+	current: CurrentWeatherType & {pollution?: number},
+	hourly: HourlyWeatherType[]
+	daily: DailyWeatherType[]
 }
 
 const initialState: initialStateType = {
-	weather: [],
-	currentDate: '',
-	currentDay: '',
-	currentTime: '',
-	currentCity: '',
-	dt: 0,
-	currentTemp: 0,
-	sunrise: '',
-	sunset: '',
-	wind: {} as WindType,
-	visibility: 0,
-	forecastList: [],
-	hourlyWeather: [],
-	airPollution: 0,
-	uv: 0,
-	feelsLike: 0,
-	humidity: 0,
-	pressure: 0
+	current: {} as CurrentWeatherType & {pollution?: number},
+	hourly: [],
+	daily: []
 };
-
 
 export const weatherSlice = createSlice({
 	name: 'weather',
 	initialState: initialState,
 	reducers: {
 		setCurrentWeather: (state, action: PayloadAction<{weather: CurrentWeatherType}>) => {
-			state.weather = action.payload.weather.weather;
-			state.currentTemp = action.payload.weather.temp;
-			state.dt = action.payload.weather.dt;
-			state.currentDate = dayjs.unix(action.payload.weather.dt).format('DD.MM.YYYY');
-			state.currentTime = dayjs.unix(action.payload.weather.dt).format('HH:mm');
-			state.currentDay = dayjs.unix(action.payload.weather.dt).format('dddd');
-			state.sunset = dayjs.unix(action.payload.weather.sunset).format('HH:mm');
-			state.sunrise = dayjs.unix(action.payload.weather.sunrise).format('HH:mm');
-			state.wind = {
-				speed: action.payload.weather.wind_speed,
-				direction: action.payload.weather.wind_deg
-			};
-			state.uv = action.payload.weather.uvi;
-			state.feelsLike = action.payload.weather.feels_like;
-			state.humidity = action.payload.weather.humidity;
-			state.visibility = action.payload.weather.visibility;
-			state.pressure = action.payload.weather.pressure;
+			state.current = action.payload.weather;
 		},
 		setTenDaysWeather: (state, action: PayloadAction<{weather: DailyWeatherType[]}>) => {
-			state.forecastList = action.payload.weather;
+			state.daily = action.payload.weather;
 		},
 		setHourlyWeather: (state, action: PayloadAction<{weather: HourlyWeatherType[]}>) => {
-			state.hourlyWeather = action.payload.weather;
+			state.hourly = action.payload.weather;
 		},
 		setAirPollution: (state, action: PayloadAction<{pollution: number}>) => {
-			state.airPollution = action.payload.pollution;
+			state.current.pollution = action.payload.pollution;
 		},
-
 	},
 });
 
