971ccae33b56fc5e022c9bbc5fee1cb138a6b0ae
Margarita P <rprivalko@gmail.com>
2024-01-23 17:49:30 +0300

    add: google api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,25 @@ import {Provider} from 'react-redux';
 import {store} from '@/app/store/store';
 import Main from '@/pages/Main';
 import '../app/globals.css';
+import '@radix-ui/themes/styles.css';
+import {Theme} from '@radix-ui/themes';
+import Script from 'next/script';
 
 
 export default function App() {
 
 	return (
-		<Provider store={store}>
-			<Main />
-		</Provider>
+		<>
+			<Provider store={store}>
+				<Theme>
+					<Main />
+				</Theme>
+			</Provider>
+			<Script
+				strategy={'beforeInteractive'}
+				src={'https://maps.googleapis.com/maps/api/js?key=AIzaSyBG--65HDrid-7sV0CXIKL7xLMXsmzLhYs&libraries=places&callback=Function.prototype'}
+			/>
+		</>
+
 	);
 }
