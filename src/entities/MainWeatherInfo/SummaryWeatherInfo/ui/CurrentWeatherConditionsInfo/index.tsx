eac2bf1e07fe9f07748315138789b1f8d088148c
Margarita P <rprivalko@gmail.com>
2024-02-09 13:01:42 +0300

    refactor: fix style

--- src/entities/MainWeatherInfo/SummaryWeatherInfo/ui/CurrentWeatherConditionsInfo/index.tsx
+++ src/entities/MainWeatherInfo/SummaryWeatherInfo/ui/CurrentWeatherConditionsInfo/index.tsx
@@ -8,10 +8,10 @@ type CurrentWeatherConditionsInfoPropsType = {
 }
 export const CurrentWeatherConditionsInfo: FC<CurrentWeatherConditionsInfoPropsType> = ({weather }) => {
 	return (
-		<>
+		<div>
 			<WeatherIcon src={weather.icon} alt={weather.icon} className={'h-9 w-9'}/>
 			<div className={'font-semibold'}>{weather.main}</div>
-		</>
+		</div>
 	);
 };
 
