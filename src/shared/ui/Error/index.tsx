d089254ee81eca6f90643603e5be9eba6b304044
Margarita P <rprivalko@gmail.com>
2024-02-10 17:25:29 +0300

    fix: error message

--- src/shared/ui/Error/index.tsx
+++ src/shared/ui/Error/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Error = () => {
 	return (
 		<div className={'flex justify-center items-center mt-24'}>
-            There was a problem loading your data. Please try again some time later!
+            There was a problem loading the weather data. Please try again some time later!
 		</div>
 	);
 };
