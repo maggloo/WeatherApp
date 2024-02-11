3ba8eb87a27864a7cfc14873fb551191d05d928f
Margarita P <rprivalko@gmail.com>
2024-02-11 17:10:58 +0300

    refactor: main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,20 +1,19 @@
 import React, {useEffect} from 'react';
-import MainWeather from '@/widgets/MainWeather';
-import {getSummaryWeather} from '@/entities/MainWeatherInfo/SummaryWeatherInfo/model';
 import {useAppDispatch, useAppSelector} from '@/app/store/store';
-import WeatherWidgets from '@/widgets/WeatherWidgets';
 import {Nav} from '@/widgets/Nav';
 import {appStatusSelector} from '@/shared/selectors/weatherSelectors';
 import {Loading} from '@/shared/ui/Loading';
 import {Error} from '@/shared/ui/Error';
+import {getStartCity} from '@/entities/MainWeatherInfo/SummaryWeatherInfo/model';
+import MainWeather from '@/widgets/MainWeather';
+import WeatherWidgets from '@/widgets/WeatherWidgets';
 
 const Main = () => {
-
-	const dispatch = useAppDispatch();
 	const status = useAppSelector(appStatusSelector);
+	const dispatch = useAppDispatch();
 
 	useEffect(() => {
-		dispatch(getSummaryWeather({lat: 53.9006, lng: 27.5590, name: 'Minsk, Belarus'}));
+		dispatch(getStartCity());
 	}, []);
 
 	return (
