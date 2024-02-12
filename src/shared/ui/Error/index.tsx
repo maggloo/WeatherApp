28abf8283ad26dbadbeeb7cc920b727e372aa853
Margarita P <rprivalko@gmail.com>
2024-02-12 18:12:47 +0300

    refactor: error page

--- src/shared/ui/Error/index.tsx
+++ src/shared/ui/Error/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {useRouter} from 'next/router';
+import {Button} from '@radix-ui/themes';
 
 export const Error = () => {
+	const {replace, reload} = useRouter();
+
+	const onClickHandler = () => {
+		void replace('/');
+		setTimeout(() => {
+			reload();
+		}, 500);
+	};
+
 	return (
-		<div className={'flex justify-center items-center mt-24'}>
-            There was a problem loading the weather data. Please try again some time later!
+		<div className={'flex justify-center flex-col items-center mt-24 gap-16'}>
+			<p>There was a problem loading the weather data.</p>
+			<Button onClick={onClickHandler}> Go back to Home </Button>
 		</div>
 	);
 };
