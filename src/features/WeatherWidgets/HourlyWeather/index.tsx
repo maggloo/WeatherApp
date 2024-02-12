19ea7a7fdb528e88f2320154979aab843b903636
Margarita P <rprivalko@gmail.com>
2024-02-12 18:13:01 +0300

    refactor: hourly weather

--- src/features/WeatherWidgets/HourlyWeather/index.tsx
+++ src/features/WeatherWidgets/HourlyWeather/index.tsx
@@ -9,9 +9,9 @@ type HourlyWeatherPropsType = {
 }
 export const HourlyWeather: FC<HourlyWeatherPropsType> = ({ hourlyWeatherArray}) => {
 	return (
-		<ToolBox className={'order-1 col-span-2 flex h-48 select-none flex-row items-center justify-between gap-12 overflow-hidden p-6 md:order-2 lg:order-3'}>
+		<ToolBox className={'order-1 col-span-2 flex h-48 select-none flex-row items-center justify-between gap-10 overflow-hidden p-6 md:order-2 lg:order-3'}>
 			{
-				hourlyWeatherArray.slice(0, 7).map((item, i) => (
+				hourlyWeatherArray.slice(0, 5).map((item, i) => (
 					<OneHourWeatherInfo key={i} item={item} index={i} />
 				))
 			}
